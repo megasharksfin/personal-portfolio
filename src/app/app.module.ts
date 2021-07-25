c67bbf44f345911b378be1d9391550959847d8dd
megasharksfin <53161366+megasharksfin@users.noreply.github.com>
2021-07-25 20:50:36 +0800

    INITIAL COMMIT - Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './_components/home/home.component';
+import { ProjectsComponent } from './_components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
